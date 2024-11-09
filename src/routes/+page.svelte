5024a1f49e624598fd512e0c0ad469e12c590d4c
Arad Fadaei <arad@fadaei.dev>
2024-11-09 14:15:19 -0500

    redo

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,25 +1,23 @@
 <script>
-
-	import RsLayout from "$lib/RsLayout/RsLayout.svelte";
-
+	import RsLayout from '$lib/RsLayout.svelte';
 </script>
 
 <div class="layout-container">
-    <RsLayout gridSize={12} />
+	<RsLayout gridSize={12} />
 </div>
 
 <style>
-    /* Reset default margins and make the layout container full viewport */
-    :global(body, html) {
-        margin: 0;
-        padding: 0;
-        width: 100%;
-        height: 100vh;
-        overflow: hidden;
-    }
+	/* Reset default margins and make the layout container full viewport */
+	:global(body, html) {
+		margin: 0;
+		padding: 0;
+		width: 100%;
+		height: 100vh;
+		overflow: hidden;
+	}
 
-    .layout-container {
-        width: 100%;
-        height: 100vh;
-    }
+	.layout-container {
+		width: 100%;
+		height: 100vh;
+	}
 </style>
